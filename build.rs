a680461718adec0111440d7ff7f48e9d315d6f2a
Kaustubh Khulbe <kkhulbe@amazon.com>
2025-07-15 16:45:42 +0000

    cargo fmt build.rs

--- build.rs
+++ build.rs
@@ -68,10 +68,7 @@ fn main() -> Result<()> {
                 "src/hotline/tests/test_lat_map.c",
                 "src/hotline/tests/test.c",
             ])
-            .includes([
-                "src/hotline",
-                "/usr/src/linux-headers-$(uname -r)/include"
-            ])
+            .includes(["src/hotline", "/usr/src/linux-headers-$(uname -r)/include"])
             .static_flag(true)
             .flag("-Werror")
             .flag("-Wextra")
